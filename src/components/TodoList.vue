14e2fefc5e83ecc64baf41a600c70bfca055dc02
Kevin Jadunandan <kevin.jadunandan@gmail.com>
2020-10-24 14:41:04 -0400

    Initial prototype of todo app.

---
+++ src/components/TodoList.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul>
+    <li v-for="item in items" :key="item.name">{{ item.name }}</li>
+  </ul>
+  <input v-model="input" @keyup.enter="addItem"/>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  name: "TodoList",
+  setup(props) {
+    console.log(props)
+
+    let items = ref([
+      {name: 'Wash Dishes'}
+    ])
+
+    let input = ref('')
+
+    const addItem = () => {
+      items.value.push({name: input.value})
+      input.value = ''
+    }
+
+    return {
+      items,
+      input,
+      addItem,
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
